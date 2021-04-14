ea2e44df22d3d65b907d69cd9fc6b56c0e2f85cb
Murat Artan <muratartan@hotmail.com>
2021-04-15 02:23:43 +0300

    create importedNode to import template and render it otomatically when class created

--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,7 @@ class ProjectInput {
    constructor() {
       this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
       this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+      const importNode = document.importNode(this.templateElement.content, true);
    }
 }
