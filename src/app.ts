94bde20457961b3c051123a31431a98fd917bbd0
Murat Artan <muratartan@hotmail.com>
2021-04-17 01:08:39 +0300

    get the output to the app screen

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,10 @@ class ProjectState {
       return this.instance;
    }
 
+   addListener(listenerFn: Function) {
+      this.listeners.push(listenerFn)
+   }
+
    addProject(title: string, description: string, numOfPeople: number) {
       const newProject = {
          id: Math.random().toString,
@@ -25,6 +29,9 @@ class ProjectState {
          people: numOfPeople
       };
       this.projects.push(newProject);
+      for (const listenerFn of this.listeners) {
+         listenerFn(this.projects.slice());
+      }
    }
 };
 
@@ -86,16 +93,33 @@ class ProjectList {
    templateElement: HTMLTemplateElement;
    hostElement: HTMLDivElement;
    element: HTMLElement;
+   assignedProjects: any[];
 
    constructor(private type: 'active' | 'finished') {
       this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
       this.hostElement = document.getElementById('app')! as HTMLDivElement;
+      this.assignedProjects = [];
 
       const importedNode = document.importNode(this.templateElement.content, true);
       this.element = importedNode.firstElementChild as HTMLElement;
       this.element.id = `${this.type}-projects`;
+
+      projectState.addListener((projects: any[]) => {
+         this.assignedProjects = projects;
+         this.renderProjects();
+      })
+
       this.attach();
       this.renderContent();
+   }
+
+   private renderProjects() {
+      const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+      for (const prjItem of this.assignedProjects) {
+         const listItem = document.createElement('li');
+         listItem.textContent = prjItem.title;
+         listEl.appendChild(listItem)
+      }
    }
 
    private renderContent() {
