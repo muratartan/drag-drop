6664f8930246dde33db66ae37dd0c0be04f695e8
Murat Artan <muratartan@hotmail.com>
2021-04-18 23:26:07 +0300

    set a list item doppable effect

--- src/app.ts
+++ src/app.ts
@@ -199,7 +199,7 @@ class ProjectItem extends Component<HTMLUListElement,HTMLLIElement> implements D
 
 // project List Class
 
-class ProjectList extends Component<HTMLDivElement,HTMLElement>{
+class ProjectList extends Component<HTMLDivElement,HTMLElement> implements DragTarget {
    
    assignedProjects: Project[];
 
@@ -210,8 +210,20 @@ class ProjectList extends Component<HTMLDivElement,HTMLElement>{
       this.configure();
       this.renderContent();
    }
+   @autobind
+   dragOverHandler(event: DragEvent) {
+      const listEl = this.element.querySelector('ul')!;
+      listEl.classList.add('droppable');
+   };
+   @autobind
+   dropHandler(_: DragEvent) {};
+   @autobind
+   dragLeaveHandler(_: DragEvent) {};
 
    configure() {
+      this.element.addEventListener('dragover', this.dragOverHandler);
+      this.element.addEventListener('dragleave', this.dragLeaveHandler);
+      this.element.addEventListener('drop', this.dropHandler);
       projectState.addListener((projects: Project[]) => {
          const relevantProjects = projects.filter(prj => {
             if (this.type === 'active') {
